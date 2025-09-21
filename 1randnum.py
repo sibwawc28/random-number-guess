b86bfea8e3a2fcfe3a8da2258b952c60980a7f40
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2025-09-21 20:43:31 +0545

    added an extra feature ie 15 tries

--- 1randnum.py
+++ 1randnum.py
@@ -3,10 +3,13 @@ import random
 number= random.randint(1,50)
 
 print("Hey bbg, guess the number between 1 and 50 okay")
+print("PS: you only get 15 tries")
 
+tries=1
 
-while 1:
+while tries<=15:
     choice=int(input("what's your guess? "))
+    tries+=1
 
     if choice < number:
         print("oops, you went low")
@@ -17,3 +20,6 @@ while 1:
     else:
         print("congrats queen")
         break
+
+else:    #python has a while-else loop
+    print("sorry queen, went of of tries")
