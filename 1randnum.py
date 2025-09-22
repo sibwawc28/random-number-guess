bff156d844a8c830dc6ba31c4a8ad7c2bcb31d14
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2025-09-22 09:01:23 +0545

    added global scoring

--- 1randnum.py
+++ 1randnum.py
@@ -1,25 +1,44 @@
 import random
+#python doesnt have do while loop
 
-number= random.randint(1,50)
+score=0
 
-print("Hey bbg, guess the number between 1 and 50 okay")
-print("PS: you only get 15 tries")
+def play ():
 
-tries=1
+    number= random.randint(1,50)
 
-while tries<=15:
-    choice=int(input("what's your guess? "))
-    tries+=1
+    print("Hey bbg, guess the number between 1 and 50 okay")
+    print("PS: you only get 15 tries")
 
-    if choice < number:
-        print("oops, you went low")
+    tries=1
+    global score
+
+    while tries<=15:
+        choice=int(input("what's your guess? "))
+        tries+=1
+
+        if choice < number:
+            print("oops, you went low")
     
-    elif choice>number:
-        print("oops, you went high")
+        elif choice>number:
+            print("oops, you went high")
     
-    else:
-        print("congrats queen")
-        break
+        else:
+            print("congrats queen")
+            score+=1
+            break
 
-else:    #python has a while-else loop
-    print("sorry queen, went of of tries")
+    else:    #python has a while-else loop
+        print("sorry queen, went out of tries. the number was", number)
+
+
+
+playing=1
+
+while (playing==1) :
+    play()
+    print("your score so far: ", score)
+    playing=int(input("enter 1 if you want to play again, 0 to stop "))
+    tries=1
+
+print(f"final score: {score}")
